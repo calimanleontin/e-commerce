4248a924d1c9cef851bc29f86d65c6517778e6ac
calimanleontin <calimanleontin@gmail.com>
2016-01-31 23:48:29 +0200

    order model added

---
+++ app/Orders.php
@@ -0,0 +1,26 @@
+<?php
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Orders extends Model
+{
+    protected $guarded= [];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function owner()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function products()
+    {
+        return $this->belongsToMany('App\Products')->withTimestamps();
+    }
+
+}
